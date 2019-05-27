df7450a1f9a215ca42b48c1f41e39105553ddb56
Carter Himmel <carter@elhnet.net>
2019-05-26 21:32:50 -0700

    updated info commands

--- src/bot/commands/util/info.ts
+++ src/bot/commands/util/info.ts
@@ -1,5 +1,6 @@
 import { Command } from 'discord-akairo';
 import { Message } from 'discord.js';
+import { stripIndents } from 'common-tags';
 
 export default class InfoCommand extends Command {
 	public constructor() {
@@ -14,12 +15,16 @@ export default class InfoCommand extends Command {
 	}
 
 	public async exec(msg: Message): Promise<Message | Message[]> {
-		const fyko = await this.client.fetchApplication().then(a => a.owner);
-		const format = fyko!.avatar && fyko!.avatar.startsWith('a_') ? 'gif' : 'png';
+		const dev = await this.client.fetchApplication().then(a => a.owner);
+		const format = dev!.avatar && dev!.avatar.startsWith('a_') ? 'gif' : 'png';
 		const embed = this.client.util.embed()
 			.setColor(this.client.config.color!)
-			.setAuthor('Bot Information', fyko!.displayAvatarURL({ format }))
-			.setDescription('Want some random animal photos in your server? [Invite](https://discordapp.com/api/oauth2/authorize?client_id=582018558759010314&permissions=116800&scope=bot) me!\nShit hit the fan? Tell me in my [Discord Server](https://discord.gg/Agg6yFV).');
+			.setAuthor('Bot Information', dev!.displayAvatarURL({ format }))
+			.setDescription(stripIndents`
+				Want some random animal photos in your server? [Invite](https://discordapp.com/api/oauth2/authorize?client_id=582018558759010314&permissions=116800&scope=bot) me!
+				Shit hit the fan? Tell me in my [Discord Server](https://discord.gg/Agg6yFV).
+				Wanna see the code? It\'s [open source](https://github.com/Fyk0/hourly-animals/)!
+			`)
 		return msg.util!.send({ embed });
 	}
 }
