7ba04b522f0bfaf3c9c17de5ae558e61dc7d32de
Carter Himmel <carter@elhnet.net>
2019-05-26 19:45:18 -0700

    just the username

--- src/bot/listeners/client/ready.ts
+++ src/bot/listeners/client/ready.ts
@@ -11,7 +11,7 @@ export default class ReadyListener extends Listener {
 
 	public async exec(): Promise<void> {
 		this.client.logger.info(`[READY] ${this.client.user!.tag} is ready to send cute puppy pics.`);
-		this.client.user!.setActivity(`@${this.client.user!.tag} help`, { type: 'WATCHING' });
+		this.client.user!.setActivity(`@${this.client.user!.username} help`, { type: 'WATCHING' });
 		setInterval(async () => {
 			for (const g2 of this.client.guilds.values()) {
 				g2.presences.clear();
